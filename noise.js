399fe1bfa9976052ea4ffd9c3564bf9351ec87bf
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-11-20 14:09:00 -0800

    implement keyboard to trigger sounds

--- noise.js
+++ noise.js
@@ -1,17 +1,47 @@
 $(document).ready( function() {
-  $('.instrument').on('click', '.note', function(e) {
-    let note = e.target.classList[1];
+  const getAudio = function getAudio(note) {
+    let id = note + 'Audio';
+    let audio = document.getElementById(id);
+    return audio;
+  };
+
+  const playNote = function playNote(note) {
     let audio = getAudio(note);
     if (audio.paused) {
       audio.play();
     } else {
       audio.currentTime = 0;
     }
+  };
+
+  $('.instrument').on('click', '.note', function(e) {
+    let note = e.target.classList[1];
+    playNote(note);
   });
 
-  const getAudio = function getAudio(note) {
-    let id = note + 'Audio';
-    let audio = document.getElementById(id);
-    return audio;
-  };
+  $('body').keydown((e) => {
+    switch (e.key) {
+      case 'z':
+        playNote('c');
+        break;
+      case 'x':
+        playNote('d');
+        break;
+      case 'c':
+        playNote('e');
+        break;
+      case 'v':
+        playNote('f');
+        break;
+      case 'b':
+        playNote('g');
+        break;
+      case 'n':
+        playNote('a');
+        break;
+      case 'm':
+        playNote('b');
+        break;
+    }
+  });
 });
