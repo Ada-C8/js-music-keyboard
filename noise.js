0def65918dac66b38cd2508a5d71a64e034c29c1
averikitsch <aekitsch@gmail.com>
2017-11-20 14:45:44 -0800

    buttons work

--- noise.js
+++ noise.js
@@ -1,3 +1,22 @@
-$(document).ready( function() {
-  // your code here
-});
+/* eslint-disable */
+const makeNote = function makeNote(n) {
+  return $(`.${n}`).on('click', function() {
+    console.log(n);
+    $(`#${n}Audio`).get(0).play();
+  });
+}
+
+
+$(document).ready(function() {
+  const playC = makeNote('c');
+  const playD = makeNote('d');
+  const playE = makeNote('e');
+  const playF = makeNote('f');
+  const playG = makeNote('g');
+  const playA = makeNote('a');
+  const playB = makeNote('b');
+
+
+
+
+})
