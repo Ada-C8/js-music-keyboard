f3aff687d06da27bcb98ac46e7708d240f91fd31
Gale Harrington <gale.harrington19@gmail.com>
2017-11-20 15:14:04 -0800

    Can play note associated with HTML element

--- noise.js
+++ noise.js
@@ -1,3 +1,9 @@
+/*eslint-disable*/
 $(document).ready( function() {
-  // your code here
+  $('.c').click( function() {
+    const classString = $(this).attr('class');
+    const letter = classString.charAt(classString.length - 1);
+    let sound = document.getElementById(`${letter}Audio`);
+    sound.play();
+  })
 });
