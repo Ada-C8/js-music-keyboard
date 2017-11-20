21f712aaf3fe1b9094456f9585ba6d317dd2498e
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-11-20 13:56:14 -0800

    buttons play sound on click

--- noise.js
+++ noise.js
@@ -1,3 +1,17 @@
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on('click', '.note', function(e) {
+    let note = e.target.classList[1];
+    let audio = getAudio(note);
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.currentTime = 0;
+    }
+  });
+
+  const getAudio = function getAudio(note) {
+    let id = note + 'Audio';
+    let audio = document.getElementById(id);
+    return audio;
+  };
 });
