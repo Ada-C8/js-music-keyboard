7c906bfd46776b9bb1566cbdc5ae240a7aa1b3b4
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-11-20 14:58:25 -0800

    changed GetElementById for jQuery and added .load() before .play()

--- noise.js
+++ noise.js
@@ -1,12 +1,14 @@
 $(document).ready(() => {
   $('.instrument').on('click', 'button', function() {
-    let audio = $(this)['context']['innerText'];
+    let audio = '#'
+     audio +=  $(this)['context']['innerText'];
     audio += 'Audio';
-    document.getElementById(audio).play();
+    $(audio)[0].load();
+    $(audio)[0].play();
   });
   $('body').keydown((event) => {
-    let audio = event.key;
-    audio += 'Audio';
-    document.getElementById(audio).play();
+    let audio = '#' + event.key + 'Audio';
+    $(audio)[0].load();
+    $(audio)[0].play();
   });
 });
