965df045c5dd4ad5da65b8cb024b4f8e27492638
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-11-21 10:36:08 -0800

    refactored the if else statement.

--- noise.js
+++ noise.js
@@ -5,29 +5,37 @@ $(document).ready(() => {
     $(`#${note}Audio`)[0].load();
     $(`#${note}Audio`)[0].play();
 
+    // must click a note before pressing keyboard buttons works
     $('body').keydown((event) => {
-      if (event.keyCode === 67) {
-        $('#cAudio')[0].load();
-        $('#cAudio')[0].play();
-      } else if (event.keyCode === 68) {
-        $('#dAudio')[0].load();
-        $('#dAudio')[0].play();
-      } else if (event.keyCode === 69) {
-        $('#eAudio')[0].load();
-        $('#eAudio')[0].play();
-      } else if (event.keyCode === 70) {
-        $('#fAudio')[0].load();
-        $('#fAudio')[0].play();
-      } else if (event.keyCode === 71) {
-        $('#gAudio')[0].load();
-        $('#gAudio')[0].play();
-      } else if (event.keyCode === 65) {
-        $('#aAudio')[0].load();
-        $('#aAudio')[0].play();
-      } else if (event.keyCode === 66) {
-        $('#bAudio')[0].load();
-        $('#bAudio')[0].play();
-      }
+      const notePressed = `#${event.key}Audio`
+      $(notePressed).get(0).load();
+      $(notePressed).get(0).play();
     });
+
+// refactored
+    // $('body').keydown((event) => {
+    //   if (event.keyCode === 67) {
+    //     $('#cAudio')[0].load();
+    //     $('#cAudio')[0].play();
+    //   } else if (event.keyCode === 68) {
+    //     $('#dAudio')[0].load();
+    //     $('#dAudio')[0].play();
+    //   } else if (event.keyCode === 69) {
+    //     $('#eAudio')[0].load();
+    //     $('#eAudio')[0].play();
+    //   } else if (event.keyCode === 70) {
+    //     $('#fAudio')[0].load();
+    //     $('#fAudio')[0].play();
+    //   } else if (event.keyCode === 71) {
+    //     $('#gAudio')[0].load();
+    //     $('#gAudio')[0].play();
+    //   } else if (event.keyCode === 65) {
+    //     $('#aAudio')[0].load();
+    //     $('#aAudio')[0].play();
+    //   } else if (event.keyCode === 66) {
+    //     $('#bAudio')[0].load();
+    //     $('#bAudio')[0].play();
+    //   }
+    // });
   });
 });
