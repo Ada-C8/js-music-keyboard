cdeb5ade2e77d9f1c96e9c6bd91200a328f111bd
Laura Robertson <mslaura.mae@gmail.com>
2017-11-20 19:45:34 -0800

    Sounds play when a user clicks on a button or uses a keyboard key

--- noise.js
+++ noise.js
@@ -1,3 +1,21 @@
-$(document).ready( function() {
-  // your code here
+//* eslint-disable */
+
+$(document).ready(() => {
+  const notes = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+  const play = (sound) => {
+    const note = `#${sound}Audio`;
+    $(note).get(0).load();
+    $(note).get(0).play();
+  };
+
+  $('.note').click(function click() {
+    play($(this).html());
+  });
+
+  $('body').keydown((event) => {
+    if (notes.includes(event.key)) {
+      play(event.key);
+    }
+  });
 });
