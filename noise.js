8d10c629eb43b5871f62bf56a7cace3b6781390e
Laura Robertson <mslaura.mae@gmail.com>
2017-11-20 20:34:26 -0800

    Removed comparison keys

--- noise.js
+++ noise.js
@@ -1,16 +1,14 @@
 $(document).ready(() => {
-  const notes = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-
   const play = (letter) => {
-    const note = `#${letter}Audio`;
-    $(note).get(0).load();
-    $(note).get(0).play();
+    const note = $(`#${letter}Audio`).get(0);
+    note.load();
+    note.play();
   };
 
   $('.note, body').on('click keydown', function player(event) {
     if (event.type === 'click') {
       play($(this).html());
-    } else if (event.type === 'keydown' && notes.includes(event.key)) {
+    } else if (event.type === 'keydown') {
       play(event.key);
     }
   });
