412362d9734d347a2e94a3c4b683f9e2ca9a5a55
enigmagnetic <lauren.cardella@gmail.com>
2017-11-21 08:24:40 -0800

    Add click and key down events to play audio files.

--- noise.js
+++ noise.js
@@ -1,3 +1,15 @@
 $(document).ready( function() {
-  // your code here
+  $('.instrument button').click(function(event) {
+    let selector = $(this).html() + 'Audio';
+    let audio = document.getElementById(selector);
+    audio.load();
+    audio.play();
+  });
+
+  $('body').keydown(function(event) {
+    let selector = event.key + 'Audio';
+    let audio = document.getElementById(selector);
+      audio.load();
+      audio.play();
+  });
 });
