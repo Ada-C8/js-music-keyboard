d7eaf1b854e70e2cf259d013bed1ae3272990612
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-11-20 16:08:41 -0800

    added randomness with space key

--- noise.js
+++ noise.js
@@ -1,14 +1,51 @@
 $(document).ready(() => {
   $('.instrument').on('click', 'button', function() {
-    let audio = '#'
-     audio +=  $(this)['context']['innerText'];
-    audio += 'Audio';
-    $(audio)[0].load();
-    $(audio)[0].play();
+    if (($(this)['context']['innerText']) === 'Suprise me') {
+      const Tags = ['aa', 'bb', 'cc']
+      const Tag = Tags[Math.floor(Math.random() * Tags.length)];
+      const Audio = '#' + Tag + 'Audio';
+      $('audio').each(function() {
+        console.log($(this)[0]);
+        $(this)[0].pause();
+      });
+
+      $(Audio)[0].load();
+      $(Audio)[0].play();
+    } else {
+      const Tag = $(this)['context']['innerText'];
+      const Audio = '#' + Tag + 'Audio';
+      $('audio').each(function() {
+        console.log($(this)[0]);
+        $(this)[0].pause();
+      });
+      $(Audio)[0].load();
+      $(Audio)[0].play();
+    }
   });
   $('body').keydown((event) => {
-    let audio = '#' + event.key + 'Audio';
-    $(audio)[0].load();
-    $(audio)[0].play();
+    const PlayedKey = event.key;
+    if (PlayedKey === ' ') {
+      const Tags = ['aa', 'bb', 'cc']
+      const Tag = Tags[Math.floor(Math.random() * Tags.length)];
+      const Audio = '#' + Tag + 'Audio';
+      $('audio').each(function() {
+        console.log($(this)[0]);
+        $(this)[0].pause();
+      });
+      $(Audio)[0].load();
+      $(Audio)[0].play();
+    } else {
+      const audio = '#' + PlayedKey + 'Audio';
+      $('audio').each(function() {
+        console.log($(this)[0]);
+        $(this)[0].pause();
+      });
+      $(audio)[0].load();
+      $(audio)[0].play();
+    }
   });
 });
+
+const RandomPlay = function RandomPlay() {
+
+}
