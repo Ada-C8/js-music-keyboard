b7791356eb7ff9f859ddeb1d0ad515916142d0e8
VirtualLindsey <tapircode@gmail.com>
2017-11-21 15:13:19 -0800

    saving on click work

--- noise.js
+++ noise.js
@@ -1,3 +1,13 @@
-$(document).ready( function() {
-  // your code here
+$(document).ready(() => {
+  const playNote = (tone) => {
+    const note = $(`#${tone}Audio`).get(0);
+    note.load();
+    note.play();
+  };
+
+  $('.note').on('click', function playSound() {
+    playNote($(this).html());
+  });
+
+  
 });
